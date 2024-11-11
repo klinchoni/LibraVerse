bf4e60890a55437c8009e018105133e747427adf
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-11 08:51:42 +0200

    Add propeties to the Book class

--- LibraVerse.Data.Models/Book.cs
+++ LibraVerse.Data.Models/Book.cs
@@ -2,6 +2,19 @@
 {
     public class Book
     {
+        //Set a new Guid
+        public Book()
+        {
+            this.Id = Guid.NewGuid();
+        }
         public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+
     }
 }
