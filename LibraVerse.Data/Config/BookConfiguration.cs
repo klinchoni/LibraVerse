c1d4ae02c0f2272a0074e2c51ef97395f034c50d
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-12 16:39:54 +0200

    Add Seeder

--- LibraVerse.Data/Config/BookConfiguration.cs
+++ LibraVerse.Data/Config/BookConfiguration.cs
@@ -33,6 +33,35 @@ namespace LibraVerse.Data.Config
                .Property(b => b.Description)
                .IsRequired()
                .HasMaxLength(DescriptionMaxLength);
+
+            builder.HasData(this.SeedBooks()); // To seed the base 
         }
+
+        private List<Book> SeedBooks()
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book()
+                {
+                    Title = "Lord of the Clans",
+                    Author = "Christie Golden",
+                    Genre = "Fantasy",
+                    ReleaseDate = new DateTime(2001, 10, 1),
+                    Description = "Raised from infancy by cruel human masters who sought to mold him into their perfect pawn, Thrall was driven by both the savagery in his heart and the cunning of his upbringing to pursue a destiny he was only beginning to understand to break his bondage and rediscover the ancient traditions of his people."
+                },
+                new Book()
+                {
+                    Title = "Bulgarcheta",
+                    Author = "Angel Karaliichev",
+                    Genre = "Fairy tales",
+                    ReleaseDate = new DateTime(1942),
+                    Description = "The collection from 1942, Bulgarcheta introduces us to the magical world of Karaliychev. Some of the stories included are set in magical and fairy-tale lands, while others are instructive tales from his time. The combination of the wisdom of folk proverbs and the author's artistic talent make the book a read that will not fail to be enjoyed by young readers."
+                }
+
+            };
+
+            return books;
+        }
+
     }
 }
