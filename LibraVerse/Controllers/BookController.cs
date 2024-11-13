0081a7d7968ff198af617670db0badc23a583f23
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-13 10:24:27 +0200

    Add book controller and book view

---
+++ LibraVerse/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using LibraVerse.Data;
+using LibraVerse.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraVerse.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly LibraDbContext dbContext;
+
+        public BookController(LibraDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Book> allBooks = this.dbContext
+                .Books
+                .ToList();
+
+            return this.View(allBooks); //allBooks is set as object in the view
+        }
+    }
+}
