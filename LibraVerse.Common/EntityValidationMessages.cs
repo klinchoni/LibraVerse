5036c422d1687eeed4cab7f198641ffb2721a35e
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-15 23:21:15 +0200

    Add error messages

---
+++ LibraVerse.Common/EntityValidationMessages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraVerse.Common
+{
+    public static class EntityValidationMessages
+    {
+        public static class Book
+        {
+            public const string TitleRequiredMessage = "Book title is required!";
+            public const string GenreRequiredMessage = "Genre is required!";
+            public const string ReleaseDateRequiredMessage = "Release date is required in format MM/yyyy";
+            public const string AuthorRequiredMessage = "Author name is required!";
+            public const string PagesRequiredMessage = "Add the number of the pages.";
+        }
+    }
+}
--- LibraVerse.Web.ViewModels/Book/AddBookInputModel.cs
+++ LibraVerse.Web.ViewModels/Book/AddBookInputModel.cs
@@ -2,24 +2,30 @@
 {
     using System.ComponentModel.DataAnnotations;
     using static LibraVerse.Common.EntityValidationConstants.Book;
+    using static LibraVerse.Common.EntityValidationMessages.Book;
     public class AddBookInputModel
     {
-        [Required]
+        public AddBookInputModel()
+        {
+            this.ReleaseDate = DateTime.UtcNow.ToString(ReleaseDateFormat);
+        }
+        [Required(ErrorMessage = TitleRequiredMessage)]
         [MaxLength(TitleMaxLength)]
         public string Title { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage = GenreRequiredMessage)]
         [MinLength(GenreMinLength)]
         [MaxLength(GenreMaxLength)]
         public string Genre { get; set; } = null!;
 
-        [Required]
-        public string ReleaseDate { get; set; } = null!;
+        [Required(ErrorMessage = ReleaseDateRequiredMessage)]
+        public string ReleaseDate { get; set; }
 
+        [Required(ErrorMessage = PagesRequiredMessage)]
         [Range(PagesMinNumber,PagesMaxNumber)]
         public int Pages {  get; set; }
 
-        [Required]
+        [Required(ErrorMessage = AuthorRequiredMessage)]
         [MinLength(NameMinLength)]
         [MaxLength(NameMaxLength)]
         public string Author { get; set; } = null!;
