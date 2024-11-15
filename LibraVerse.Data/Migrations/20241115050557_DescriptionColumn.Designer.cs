410c7019a74383cec75b3ca1fade93a38740f690
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-15 07:25:23 +0200

    Add Migration for the new column

--- LibraVerse.Data/Migrations/20241115050557_DescriptionColumn.Designer.cs
+++ LibraVerse.Data/Migrations/20241115050557_DescriptionColumn.Designer.cs
@@ -4,6 +4,7 @@ using LibraVerse.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraVerse.Data.Migrations
 {
     [DbContext(typeof(LibraDbContext))]
-    partial class LibraDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241115050557_DescriptionColumn")]
+    partial class DescriptionColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
