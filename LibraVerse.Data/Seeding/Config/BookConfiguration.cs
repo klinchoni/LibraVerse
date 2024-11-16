e26de94da10bb578e9e62024ed19c34de414a3ae
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-16 23:06:27 +0200

    update

--- LibraVerse.Data/Seeding/Config/BookConfiguration.cs
+++ LibraVerse.Data/Seeding/Config/BookConfiguration.cs
@@ -1,73 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using LibraVerse.Data.Models.Books;
-using static LibraVerse.Common.EntityValidationConstants.Book;
-
-namespace LibraVerse.Data.Seeding.Config
+﻿namespace LibraVerse.Data.Seeding.Config
 {
-    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using LibraVerse.Data.Models.Books;
+
+    internal class BookConfiguration : IEntityTypeConfiguration<Book>
     {
         public void Configure(EntityTypeBuilder<Book> builder)
         {
-            //Fluent API
+            var data = new DataSeed();
 
-            builder.HasKey(b => b.Id);
-
-            builder
-                 .Property(b => b.Title)
-                 .IsRequired()
-                 .HasMaxLength(TitleMaxLength);
-
-            builder
-               .Property(b => b.Author)
-               .IsRequired()
-               .HasMaxLength(NameMaxLength);
-
-            builder
-                .Property(b => b.Genre)
-                .IsRequired()
-                .HasMaxLength(GenreMaxLength);
-
-            builder
-               .Property(b => b.Pages)
-               .IsRequired()
-               .HasMaxLength((int)BookPageMaxValue);
-
-            builder
-                .Property(b => b.Description)
-                .IsRequired()
-                .HasMaxLength(DescriptionMaxLength);
-
-            builder.HasData(SeedBooks()); // To seed the base 
-        }
-
-        private List<Book> SeedBooks()
-        {
-            List<Book> books = new List<Book>()
+            builder.HasData(new Book[]
             {
-                new Book()
-                {
-                    Title = "Лорд на клановете",
-                    Author = "Кристи Голдън",
-                    Genre = "Фентъзи",
-                    ReleaseDate = new DateTime(2001, 10, 1),
-                    Pages = 278,
-                    Description = "Лорд на клановете разказва историята за възхода на вожд Трал към славата след краха на Ордата. Това е адаптация на отменената игра на Blizzard Warcraft Adventures: Lord of the Clans. Препубликуван е през 2016 г. за поредицата Blizzard Legends."
-                },
-                new Book()
-                {
-                    Title = "Българчета",
-                    Author = "Ангел Каралийчев",
-                    Genre = "Разкази и приказки за деца",
-                    ReleaseDate = new DateTime(1942),
-                    Pages = 192,
-                    Description = "Сборникът от 1942 г. „Българчета“ ни въвежда в магическия свят на Каралийчев. Част от поместените истории се развиват във вълшебни и приказни страни, докато други са поучителни разкази от неговото съвремие.\r\n\r\nСъчетанието от мъдростта на народните притчи и художествения талант на автора правят книгата четиво, което няма как да не бъде харесано от малките читатели.\r\n\r\nЗащото без значение в каква епоха живеем, децата си остават деца, а основните морални ценности са непреходни."
-                }
-            };
-
-
-            return books;
+                data.BookOne,
+                data.BookTwo,
+                data.BookThree,
+                data.BookFour,
+                data.BookFive,
+                data.BookSix,
+                data.BookSeven,
+                data.BookEight,
+                data.BookNine,
+                data.BookTen,
+                data.BookEleven,
+                data.BookTwelve,
+                data.BookThirteen,
+                data.BookFourteen,
+                data.BookFiveteen,
+                data.BookSixteen,
+                data.BookSeventeen
+            });
         }
-
     }
 }
