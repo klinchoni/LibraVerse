e17287360626c3b7a6c0739b5adbeee191b5cef6
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-16 20:12:57 +0200

    Add Book Store

---
+++ LibraVerse.Data.Models/BookStores/BookStore.cs
@@ -0,0 +1,43 @@
+﻿using LibraVerse.Data.Models.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static LibraVerse.Common.EntityValidationConstants.BookStore;
+
+namespace LibraVerse.Data.Models.BookStores
+{
+    public class BookStore
+    {
+        [Comment("The BookStore's Identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(BookStoreNameMaxLength)]
+        [Comment("The BookStore's Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(BookStoreLocationMaxLength)]
+        [Comment("The BookStore's Location")]
+        public string Location { get; set; } = null!;
+
+        [Required]
+        [Comment("The BookStore's Mobile Contact")]
+        public string Contact { get; set; } = null!;
+
+        [Required]
+        [Comment("The BookStore's Opening Time")]
+        public DateTime OpeningTime { get; set; }
+
+        [Required]
+        [Comment("The BookStore's Closing Time")]
+        public DateTime ClosingTime { get; set; }
+
+        [Required]
+        [MaxLength(BookStoreImageUrlMaxLength)]
+        [Comment("The BookStore's Image Url")]
+        public string ImageUrl { get; set; } = null!;
+
+        public ICollection<BookBookStore> BooksBookStores { get; set; } = new HashSet<BookBookStore>();
+
+    }
+}
