67e8a09397f93e227968aecb13d3efcc1c700661
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-16 20:13:25 +0200

    Add cart

---
+++ LibraVerse.Data.Models/Carts/Cart.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using LibraVerse.Data.Models.Mappings;
+using LibraVerse.Data.Models.Roles;
+
+namespace LibraVerse.Data.Models.Carts
+{
+    public class Cart
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Comment("The User's Identifier")]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        [Comment("The current User")]
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<BookCart> BooksCarts { get; set; } = new HashSet<BookCart>();
+        public ICollection<EventCart> EventsCarts { get; set; } = new HashSet<EventCart>();
+    }
+}
