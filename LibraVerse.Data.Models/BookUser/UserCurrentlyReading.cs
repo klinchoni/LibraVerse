3b1be1d6826a3317864bf6bec9bdeb0d4cc6d2ac
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-16 20:13:13 +0200

    Add book user for reading

---
+++ LibraVerse.Data.Models/BookUser/UserCurrentlyReading.cs
@@ -0,0 +1,37 @@
+﻿namespace LibraVerse.Data.Models.BookUser
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using LibraVerse.Data.Models.Books;
+    using LibraVerse.Data.Models.Roles;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class UserCurrentlyReading
+    {
+        [Required]
+        [Comment("The current Book's Identifier")]
+        public Guid BookId { get; set; }
+
+        [ForeignKey(nameof(BookId))]
+        [Comment("The current Book")]
+        public Book Book { get; set; } = null!;
+
+
+        [Required]
+        [Comment("The current User's Identifier")]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        [Comment("The current User")]
+        public ApplicationUser User { get; set; } = null!;
+
+        [Required]
+        [Comment("The Page the User is on to")]
+        public int CurrentPage { get; set; }
+
+        [Required]
+        [Comment("The time when the entity was added")]
+        public DateTime TimeAdded { get; set; }
+    }
+}
