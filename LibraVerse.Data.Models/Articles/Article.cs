50fa47c567a3f391d9d0de1eac60eef2d085734f
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-16 20:12:05 +0200

    Add Article

---
+++ LibraVerse.Data.Models/Articles/Article.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static LibraVerse.Common.EntityValidationConstants.Article;
+
+
+namespace LibraVerse.Data.Models.Articles
+{
+    public class Article
+    {
+        [Key]
+        [Comment("The current Article's Identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(ArticleTitleMaxLength)]
+        [Comment("The current Article's Title")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ArticleContentMaxLength)]
+        [Comment("The current Article's Content")]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        [Comment("The date on which the current Article was posted")]
+        public DateTime DatePublished { get; set; }
+
+        [Required]
+        [MaxLength(ArticleImageUrlMaxLength)]
+        [Comment("The current Article's Image Url")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Comment("The current Article's Views Count")]
+        public int ViewsCount { get; set; }
+
+        public ICollection<ArticleComment> Comments { get; set; } = new HashSet<ArticleComment>();
+    }
+}
---
+++ LibraVerse.Data.Models/Articles/ArticleComment.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static LibraVerse.Common.EntityValidationConstants.ArticleComment;
+
+namespace LibraVerse.Data.Models.Articles
+{
+    public class ArticleComment
+    {
+        [Key]
+        [Comment("The current Article Comment's Identifier")]
+        public Guid Id { get; set; }
+
+        [MaxLength(ArticleCommentTitleMaxLength)]
+        [Comment("The current Article Comment's Title")]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(ArticleCommentDescriptionMaxLength)]
+        [Comment("The current Article Comment's Description")]
+        public string Description { get; set; } = null!;
+
+
+        [Required]
+        [Comment("The current Article's Identifier")]
+        public int ArticleId { get; set; }
+
+        [ForeignKey(nameof(ArticleId))]
+        [Comment("The current Article")]
+        public Article Article { get; set; } = null!;
+
+        [Required]
+        [Comment("The current User's Identifier")]
+        public string UserId { get; set; } = null!;
+
+       // [ForeignKey(nameof(UserId))]
+       // [Comment("The current User")]
+        //public ApplicationUser User { get; set; } = null!;
+    }
+}
