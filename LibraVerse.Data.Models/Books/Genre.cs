677b72bf1b23629f7657f41a9cd8221ba154d784
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-25 09:18:31 +0200

    Add prop for book's genre

--- LibraVerse.Data.Models/Books/Genre.cs
+++ LibraVerse.Data.Models/Books/Genre.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using static LibraVerse.Common.EntityValidationConstants.Genre;
-namespace LibraVerse.Data.Models.Books
+﻿namespace LibraVerse.Data.Models.Books
 {
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using static LibraVerse.Common.Constants.EntityValidationConstants.Genre;
     public class Genre
     {
         [Key]
-        [Comment("The Genre's Identifier")]
+        [Comment("The Identifier of the Genre")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(GenreNameMaxLength)]
-        [Comment("The Genre's Name")]
+        [Comment("The Name of the Genre")]
         public string Name { get; set; } = null!;
 
         public ICollection<Book> Books { get; set; } = new HashSet<Book>();
