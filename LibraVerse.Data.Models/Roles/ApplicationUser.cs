4bdd3fb1c601d3c8ca28bd5aa8202e3bae0e43af
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-25 15:49:37 +0200

    Add Role for Application User

--- LibraVerse.Data.Models/Roles/ApplicationUser.cs
+++ LibraVerse.Data.Models/Roles/ApplicationUser.cs
@@ -2,24 +2,19 @@
 {
     using Microsoft.AspNetCore.Identity;
     using System.ComponentModel.DataAnnotations;
-    using static LibraVerse.Common.EntityValidationConstants.ApplicationUser;
+    using static LibraVerse.Common.Constants.EntityValidationConstants.ApplicationUserConstant;
 
     public class ApplicationUser : IdentityUser
     {
         [Required]
         [PersonalData]
         [MaxLength(ApplicationUserFirstNameMaxLength)]
-        public string FirstName { get; set; } = string.Empty;
+        public string FirstName{ get; set; } = string.Empty;
 
         [Required]
         [PersonalData]
         [MaxLength(ApplicationUserLastNameMaxLength)]
         public string LastName { get; set; } = string.Empty;
-    }
-
-    public static class PublisherConstants
-    {
-        public const string PublisherEmailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
     }
 
 }
