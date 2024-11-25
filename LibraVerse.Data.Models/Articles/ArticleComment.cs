2cef23db9258c21b001503ddf8c21b1667ea6a6f
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-25 09:17:30 +0200

    Add prop for Article Comment

--- LibraVerse.Data.Models/Articles/ArticleComment.cs
+++ LibraVerse.Data.Models/Articles/ArticleComment.cs
@@ -1,42 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using static LibraVerse.Common.EntityValidationConstants.ArticleComment;
-using LibraVerse.Data.Models.Roles;
-
-namespace LibraVerse.Data.Models.Articles
+﻿namespace LibraVerse.Data.Models.Articles
 {
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using static LibraVerse.Common.Constants.EntityValidationConstants.ArticleComment;
+    using LibraVerse.Data.Models.Roles;
     public class ArticleComment
     {
         [Key]
-        [Comment("The current Article Comment's Identifier")]
-        public Guid Id { get; set; }
+        [Comment("The Identifier of Article Comment")]
+        public int Id { get; set; }
 
         [MaxLength(ArticleCommentTitleMaxLength)]
-        [Comment("The current Article Comment's Title")]
+        [Comment("The Article Comment's Title")]
         public string Title { get; set; } = null!;
 
         [MaxLength(ArticleCommentDescriptionMaxLength)]
-        [Comment("The current Article Comment's Description")]
+        [Comment("The Article Comment's Description")]
         public string Description { get; set; } = null!;
 
-
         [Required]
-        [Comment("The current Article's Identifier")]
+        [Comment("The Article's Identifier")]
         public int ArticleId { get; set; }
 
         [ForeignKey(nameof(ArticleId))]
-        [Comment("The current Article")]
+        [Comment("The Article")]
         public Article Article { get; set; } = null!;
 
         [Required]
-        [Comment("The current User's Identifier")]
+        [Comment("The Identifier of the User")]
         public string UserId { get; set; } = null!;
 
         [ForeignKey(nameof(UserId))]
-        [Comment("The current User")]
+        [Comment("The User")]
         public ApplicationUser User { get; set; } = null!;
     }
 }
