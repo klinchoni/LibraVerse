33c9db0c4692c9389ed0d8949ec0e19c2600a68d
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-11-26 15:05:15 +0200

    Added events

--- LibraVerse.Data.Models/Events/Event.cs
+++ LibraVerse.Data.Models/Events/Event.cs
@@ -1,50 +1,49 @@
-﻿using LibraVerse.Data.Models.Mappings;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using static LibraVerse.Common.EntityValidationConstants.Event;
-
-namespace LibraVerse.Data.Models.Events
+﻿namespace LibraVerse.Data.Models.Events
 {
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using static LibraVerse.Common.Constants.EntityValidationConstants.Event;
+    using LibraVerse.Data.Models.Mappings;
     public class Event
     {
         [Key]
-        [Comment("The current Event's Identifier")]
-        public Guid Id { get; set; }
+        [Comment("The Identifier of the Event")]
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(EventTopicMaxLength)]
-        [Comment("The current Event's Topic")]
+        [Comment("The Topic of the Event")]
         public string Topic { get; set; } = null!;
 
         [Required]
         [MaxLength(EventDescriptionMaxLength)]
-        [Comment("The current Event's Description")]
+        [Comment("The Description of the Event")]
         public string Description { get; set; } = null!;
 
         [Required]
         [MaxLength(EventLocationMaxLength)]
-        [Comment("The current Event's Location")]
+        [Comment("The Location of the Event")]
         public string Location { get; set; } = null!;
 
         [Required]
-        [Comment("The current Event's start date and hour")]
+        [Comment("The start date and start hour of the Event")]
         public DateTime StartDate { get; set; }
 
         [Required]
-        [Comment("The current Event's end date and hour")]
+        [Comment("The end date and the end hour of the Event")]
         public DateTime EndDate { get; set; }
 
         [Required]
-        [Comment("The current Event's seats")]
+        [Comment("The seats of the Event")]
         public int Seats { get; set; }
 
         [Required]
-        [Comment("The current Event's Ticket Price")]
+        [Comment("The Price of the Ticet on the Event")]
         public decimal TicketPrice { get; set; } = 10;
 
         [Required]
         [MaxLength(EventImageUrlMaxLength)]
-        [Comment("The current Event's Image Url")]
+        [Comment("The Image Url of the Event")]
         public string ImageUrl { get; set; } = null!;
 
         public ICollection<EventParticipant> EventsParticipants { get; set; } = new HashSet<EventParticipant>();
