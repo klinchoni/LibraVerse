dd6779551679e0daed23b9b53ed837e24799ae4c
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-12-03 15:49:06 +0200

    Added BookBookStore

---
+++ LibraVerse.Data.Models/Mappings/BookBookStore.cs
@@ -0,0 +1,28 @@
+﻿namespace LibraVerse.Data.Models.Mappings
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.EntityFrameworkCore;
+    using LibraVerse.Data.Models.Books;
+    using LibraVerse.Data.Models.BookStores;
+
+    public class BookBookStore
+    {
+        [Required]
+        [Comment("The current Book's Identifier")]
+        public int BookId { get; set; }
+
+        [ForeignKey(nameof(BookId))]
+        [Comment("The current Book")]
+        public Book Book { get; set; } = null!;
+
+
+        [Required]
+        [Comment("The current BookStore's Identifier")]
+        public int BookStoreId { get; set; }
+
+        [ForeignKey(nameof(BookStoreId))]
+        [Comment("The current BookStore")]
+        public BookStore BookStore { get; set; } = null!;
+    }
+}
