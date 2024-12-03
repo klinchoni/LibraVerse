5ad00b33fcc3491c440a3af67be624c4e89d95d0
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-12-03 15:58:40 +0200

    UpDATED

--- LibraVerse.Core/Models/ViewModels/Event/EventAddViewModel.cs
+++ LibraVerse.Core/Models/ViewModels/Event/EventAddViewModel.cs
@@ -2,8 +2,8 @@
 {
     using LibraVerse.Core.Contracts;
     using System.ComponentModel.DataAnnotations;
-    using static LibraVerse.Data.Constants.DataConstants;
-    using static LibraVerse.Data.Constants.DataConstants.EventConstants;
+    using static LibraVerse.Common.EntityValidationMessages.Data;
+    using static LibraVerse.Common.Constants.EntityValidationConstants.Event;
 
     public class EventAddViewModel : IEventModel
     {
