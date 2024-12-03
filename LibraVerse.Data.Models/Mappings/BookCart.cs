5ee0c3189872258134c3cc712aab54f3620e6a27
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-12-03 15:49:33 +0200

    Added BookCart

--- LibraVerse.Data.Models/Mappings/BookCart.cs
+++ LibraVerse.Data.Models/Mappings/BookCart.cs
@@ -1,26 +1,37 @@
 ﻿namespace LibraVerse.Data.Models.Mappings
 {
     using Microsoft.EntityFrameworkCore;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.ComponentModel.DataAnnotations;
     using LibraVerse.Data.Models.Books;
     using LibraVerse.Data.Models.Carts;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class BookCart
     {
         [Required]
-        [Comment("The Identifier of the Book")]
+        [Comment("The current Book's Identifier")]
         public int BookId { get; set; }
 
         [ForeignKey(nameof(BookId))]
-        [Comment("The Book")]
+        [Comment("The current Book")]
         public Book Book { get; set; } = null!;
 
         [Required]
-        [Comment("The Identifier of the Cart")]
+        [Comment("The current Cart's Identifier")]
         public int CartId { get; set; }
 
         [ForeignKey(nameof(CartId))]
-        [Comment("The Cart")]
+        [Comment("The current Cart")]
         public Cart Cart { get; set; } = null!;
+
+        // Добавено свойство Quantity, което ще показва количеството на събитието в количката
+        [Required]
+        [Comment("The quantity of the book in the cart")]
+        public int Quantity { get; set; }
+
+        // Добавено свойство за дата на добавяне на книгата в количката
+        [Required]
+        [Comment("Date when the book was added to the cart")]
+        public DateTime DateAdded { get; set; } = DateTime.Now; // Може да се зададе по подразбиране на текущата дата и час
     }
 }
