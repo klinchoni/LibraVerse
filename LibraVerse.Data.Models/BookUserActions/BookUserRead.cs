4883f0cdcb52b9b71555ddbb4cd3ae1104afd66c
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-12-03 15:52:05 +0200

    Updated the book that user read

---
+++ LibraVerse.Data.Models/BookUserActions/BookUserRead.cs
@@ -0,0 +1,33 @@
+﻿namespace LibraVerse.Data.Models.BookUserActions
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using LibraVerse.Data.Models.Books;
+    using LibraVerse.Data.Models.Roles;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class BookUserRead
+    {
+        [Required]
+        [Comment("The current Book's Identifier")]
+        public int BookId { get; set; }
+
+        [ForeignKey(nameof(BookId))]
+        [Comment("The current Book")]
+        public Book Book { get; set; } = null!;
+
+
+        [Required]
+        [Comment("The current User's Identifier")]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        [Comment("The current User")]
+        public ApplicationUser User { get; set; } = null!;
+
+        [Required]
+        [Comment("The time when the entity was added")]
+        public DateTime TimeAdded { get; set; }
+    }
+}
